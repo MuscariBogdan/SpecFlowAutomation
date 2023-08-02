fc9b937bb1db54ad08a763e928fdeadf7cccf261
Muscari Bogdan <99813280+MuscariBogdan@users.noreply.github.com>
2023-08-02 19:44:15 +0300

    Update HomePage.cs

--- SpecFlowAutomation/LoginAutomation/PageObjects/HomePage.cs
+++ SpecFlowAutomation/LoginAutomation/PageObjects/HomePage.cs
@@ -13,14 +13,19 @@ namespace LoginAutomation.PageObjects
 
         public void ClickLogoutButton()
         {
+            ClickBurgerMenu();
+
+            IWebElement logoutButton = driver.FindElement(By.XPath("//a[@id='logout_sidebar_link']"));
+            logoutButton.Click();
+        }
+
+        public void ClickBurgerMenu()
+        {
             IWebElement burgerMenu = driver.FindElement(By.XPath("//button[@id='react-burger-menu-btn']"));
 
             burgerMenu.Click();
 
             Thread.Sleep(1500);
-
-            IWebElement logoutButton = driver.FindElement(By.XPath("//a[@id='logout_sidebar_link']"));
-            logoutButton.Click();
         }
 
         /*public ProductListingPage GoToProductListingPage()
