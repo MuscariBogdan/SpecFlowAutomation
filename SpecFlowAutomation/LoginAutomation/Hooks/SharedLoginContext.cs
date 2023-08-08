0ee70189d61ae9b8a8f342cf3fdc2527fa271299
MuscariBogdan <99813280+MuscariBogdan@users.noreply.github.com>
2023-08-08 23:32:45 +0300

    Added hooks to ease up the Login

---
+++ SpecFlowAutomation/LoginAutomation/Hooks/SharedLoginContext.cs
@@ -0,0 +1,23 @@
+﻿using LoginAutomation.PageObjects;
+using OpenQA.Selenium;
+
+namespace LoginAutomation
+{
+    internal class SharedLoginContext
+    {
+        private IWebDriver _driver;
+        private LoginPage _loginPage;
+        private HomePage _homePage;
+
+        public SharedLoginContext(IWebDriver driver)
+        {
+            _driver = driver;
+            _loginPage = new LoginPage(_driver);
+            _homePage = _loginPage.PerformLogin("standard_user", "secret_sauce");
+        }
+
+        public IWebDriver Driver => _driver;
+        public LoginPage LoginPage => _loginPage;
+        public HomePage HomePage => _homePage;
+    }
+}
